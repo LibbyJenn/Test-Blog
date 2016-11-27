95d7e9e2c6a6426d0d938aabba6cff2f7afa4ece
Libby Jennings <libbyj@nz1.ibm.com>
2016-11-27 16:08:31 -0600

    added index and db code

--- index.php
+++ index.php
@@ -1,21 +1,63 @@
+<?php
+    session_start();
+    include_once("db.php");
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+    <title>Blog</title>
 </head>
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+    <h1>Blog Posts</h1>
+    <?php
+    require_once("nbbc/nbbc.php");
+
+    $bbcode = new BBCode();
+
+    $sql = "SELECT * FROM posts ORDER BY id DESC";
+
+    $res = mysqli_query($db, $sql) or die (mysqli_error());
+
+    $posts = "";
+
+    if(mysqli_num_rows($res)> 0) {
+
+        while($row = mysqli_fetch_assoc($res)){
+
+            $id = $row['id'];
+            $title = $row['title'];
+            $content = $row['content'];
+            $date = $row['date'];
+
+            $output = $bbcode->Parse($content);
+
+            $posts .= "<div><h2><a href='view_post.php?pid=$id'>$title</a></h2><h3>$date</h3><p>$output</p><hr /></div>";
+        }
+
+        echo $posts;
+    } else {
+
+        echo "There are no posts to display";
+    }
+
+    if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
+
+        echo "<a href='admin.php'>Admin</a> | <a href='logout.php'>Log Out</a>";
+    }
+
+    if(!isset($_SESSION['username'])) {
+
+        echo "<a href='login.php'>Login</a>";
+    }
+
+    if(isset($_SESSION['username']) && !isset($_SESSION['admin'])) {
+
+        echo "<a href='logout.php'>Logout</a>";
+    }
+    ?>
+
+
 </body>
 </html>
